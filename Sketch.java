be6a0bc5cd611ead93387f5f386792da1ba00514
linsteve25 <steve.lin25@ycdsbk12.ca>
2024-06-16 01:09:32 -0400

    Added more scenes, perfected the buttons & timer, completed end page, bug fixes, added mixed & pop culture questions

--- Sketch.java
+++ Sketch.java
@@ -7,6 +7,8 @@ public class Sketch extends PApplet {
   PImage imgBackground;
   PImage imgOpeningBackground;
   PImage imgTopicsBackground;
+  PImage imgTimerBackground;
+  PImage imgEndingBackground;
 
   String[] strHistoryQuestions;
   String[][] strHistoryOptions;
@@ -19,10 +21,18 @@ public class Sketch extends PApplet {
   String[][] strGeographyOptions;
   int[] intCorrectAnswerGeography;
 
+  String[] strPopQuestions;
+  String[][] strPopOptions;
+  int[] intCorrectAnswerPop;
+
+  String[] strMixedQuestions;
+  String[][] strMixedOptions;
+  int[] intCorrectAnswerMixed;
+
   int intPage = 0;
-  int intHistoryOrGeography = 0; // 0 - history; 1 - geography
+  int intHistoryOrGeography = 0; 
   int intTimer = 2000;
-  int previousTime = 0;
+  int intPreviousTime = 0;
   boolean blnPauseTimer = false;
 
   /**
@@ -38,21 +48,16 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    // load image
-    imgBackground = loadImage("f3989e431422a23123fc19fcc43f8d2e.jpeg");
-
-    // resize the image to fit the background
-    imgBackground.resize(width, height);
-    
+ 
     // array for the history questions 
     strHistoryQuestions = new String[] {
       "What year is was John F. Kennedy assassinated?", 
       "Who was the first President of the United States?",
       "What year did the Titanic sink?",
-      "Who was the Egyptian queen known for her relationships with Julius Caesar and Mark Antony?",
+      "A queen known for her relationships with Julius Caesar",
       "Who was the leader of the Soviet Union during World War II?",
       "Who was the first emperor of Rome?",
-      "The Great Wall of China was primarily built to protect against invasions from which group?",
+      "The Great Wall of China was built to protect against invasions from...",
       "Which treaty ended World War I?",
       "Which empire was ruled by Genghis Khan?",
       "Who was the principal author of the Declaration of Independence?"
@@ -78,7 +83,7 @@ public class Sketch extends PApplet {
     // set question number to 0
     intQuestionNumber = 0;
 
-    // array for the answers of the geography questions
+    // array for geography questions
     strGeographyQuestions = new String[] {
       "What is the capital city of Australia?",
       "Which river is the longest in the world?",
@@ -108,6 +113,68 @@ public class Sketch extends PApplet {
 
     // array for the correct answer index for geography questions
     intCorrectAnswerGeography = new int[]{2, 0, 2, 2, 0, 3, 1, 1, 2, 1};
+
+    // array for the pop culture questions
+    strPopQuestions = new String[] {
+      "Who won the Grammy Award for Album of the Year in 2023?",
+      "What is the name of the fourth studio album by Taylor Swift, released in 2012?",
+      "Who played the role of Iron Man in the Marvel Cinematic Universe?",
+      "Which artist released the hit single \"Blinding Lights\" in 2019?",
+      "Which book series is written by J.K. Rowling?",
+      "What is the highest-grossing film of all time as of 2023?",
+      "Which band released the album \"Abbey Road\"?",
+      "Who played the character of Katniss Everdeen in \"The Hunger Games\" film series?",
+      "Which K-pop group released the hit song \"Dynamite\" in 2020?",
+      "Who wrote the novel \"To Kill a Mockingbird\"?"
+    };
+
+    // array for the options of pop culture questions
+    strPopOptions = new String [][] {
+      {"a) Harry Styles", "b) Beyoncé", "c) Taylor Swift", "d) Billie Eilish"},
+      {"a) Fearless", "b) Speak Now", "c) Red", "d) 1989"},
+      {"a) Chris Hemsworth", "b) Chris Evans", "c) Robert Downey Jr.", "d) Mark Ruffalo"},
+      {"a) Post Malone", "b) The Weeknd", "c) Drake", "d) Ed Sheeran"},
+      {"a) The Hunger Games", "b) Twilight", "c) Harry Potter", "d) Percy Jackson"},
+      {"a) Avatar", "b) Avengers: Endgame", "c) Titanic", "d) Star Wars: The Force Awakens"},
+      {"a) The Rolling Stones", "b) The Beatles", "c) Pink Floyd", "d) Led Zeppelin"},
+      {"a) Jennifer Lawrence", "b) Emma Watson", "c) Kristen Stewart", "d) Shailene Woodley"},
+      {"a) BLACKPINK", "b) BTS", "c) EXO", "d) TWICE"},
+      {"a) Harper Lee", "b) F. Scott Fitzgerald", "c) J.D. Salinger", "d) Mark Twain"}
+    };
+
+    // array for the correct answer index for the pop culture questions
+    intCorrectAnswerPop = new int[] {0, 2, 2, 1, 2, 0, 1, 0, 1, 0};
+
+    // array for the questions of mixed genres
+    strMixedQuestions = new String[] {
+      "Which country is known as the Land of the Rising Sun?",
+      "What is the smallest prime number?",
+      "What is the largest organ in the human body?",
+      "Which famous scientist developed the theory of relativity?",
+      "In which year did World War II end?",
+      "What is the currency of the United Kingdom?",
+      "What is the main ingredient in traditional Japanese miso soup?",
+      "Which element is known for its distinctive yellow-green gas at room temperature?",
+      "Which element is used in pencils for writing?",
+      "Which country hosted the 2016 Summer Olympics?"
+    };
+
+    // array for the options of mixed genres
+    strMixedOptions = new String[][] {
+      {"a) China", "b) Japan", "c) South Korea", "d) Thailand"},
+      {"a) 1", "b) 2", "c) 3", "d) 5"},
+      {"a) Heart", "b) Liver", "c) Skin", "d) Lungs"},
+      {"a) Isaac Newton", "b) Galileo Galilei", "c) Mr. Fabroa", "d) Albert Einstein"},
+      {"a) 1940", "b) 1943", "c) 1945", "d) 1950"},
+      {"a) Dollar", "b) Euro", "c) Pound Sterling", "d) Yen"},
+      {"a) Tofu", "b) Seaweed", "c) Miso paste", "d) Rice"},
+      {"a) Fluorine", "b) Chlorine", "c) Bromine", "d) Iodine"},
+      {"a) Graphite", "b) Charcoal", "c) Lead", "d) Carbon"},
+      {"a) China", "b) United Kingdom", "c) Brazil", "d) Japan"}
+    };
+    
+    intCorrectAnswerMixed = new int[]{1, 1, 2, 3, 2, 2, 2, 1, 0, 2};
+
   }
 
   public void draw() {
@@ -115,16 +182,16 @@ public class Sketch extends PApplet {
     if (intPage == 0){
       drawOpeningPage();
     }
-    if (intPage == 1) {
+    else if (intPage == 1) {
       drawTopicsPage();
     }
-    if (intPage == 2) {
-      drawStartingPage();
+    else if (intPage == 2) {
+      drawTimerPage();
     }
-    if (intPage == 3) {
+    else if (intPage == 3) {
       drawQuestionPage();
     }
-    if (intPage == 4) {
+    else if (intPage == 4) {
       drawFinalPage();
     }
   
@@ -143,89 +210,17 @@ public class Sketch extends PApplet {
 
   public void drawTopicsPage() {
 
-    imgTopicsBackground = loadImage ("History.jpg");
+    imgTopicsBackground = loadImage ("Mode Page.jpg");
 
     image(imgTopicsBackground, 0, 0);
+
   }
-  public void drawStartingPage() {
 
-    // draw the background
-    image(imgBackground, 0, 0);
+  public void drawTimerPage() {
 
-    textSize(50);
-    text("Select which topic you would like to play", 80, 100);
+    imgTimerBackground = loadImage ("Difficulty Page.jpg");
 
-    // print red button
-    fill(255, 0, 0);
-    rect(150, 150, 300, 60, 30);
-    fill(255, 255, 255);
-    textSize(22);
-    text("Geography", 250, 190);
- 
-    // print green button
-    fill(0, 255, 0);
-    rect(600, 150, 300, 60, 30);
-    fill(255, 255, 255);
-    textSize(22);
-    text("History", 725, 190);
- 
-    textSize(50);
-    text("Select the timer", 80, 280);
-
-    // print 2s button
-    fill(0, 0, 0);
-    rect(150, 300, 150, 60, 30);
-    fill(255, 255, 255);
-    textSize(22);
-    text("2s", 210, 330);
-
-    // print 5s button
-    fill(0, 0, 0);
-    rect(350, 300, 150, 60, 30);
-    fill(255, 255, 255);
-    textSize(22);
-    text("5s", 410, 330);
-
-    // print 10 button
-    fill(0, 0, 0);
-    rect(550, 300, 150, 60, 30);
-    fill(255, 255, 255);
-    textSize(22);
-    text("10s", 600, 330);    
-
-    // print next button
-    fill(0, 255, 255);
-    rect(400, 500, 300, 60, 30);
-    fill(255, 255, 255);
-    textSize(22);
-    text("Next Page", 500, 535);
-    // set timer
-  } 
-
-  public void drawFinalPage() {
-    intHistoryOrGeography = 0; 
-    intTimer = 2000;
-    intQuestionNumber = 0;
-    image(imgBackground, 0, 0);
-
-    // print the questions
-    fill(255);
-    textSize(50);
-    text("Your score is " + intScore, 200, 220);
-    
-    // print red button
-    fill(255, 0, 0);
-    rect(200, 300, 200, 100, 30);
-    fill(255, 255, 255);
-    textSize(30);
-    text("Play again", 230, 350);
-
-    // print green button
-    fill(0, 255, 0);
-    rect(600, 300, 200, 100, 30);
-    fill(255, 255, 255);
-    textSize(30);
-    text("Quit", 670, 350);
+    image(imgTimerBackground, 0, 0);
 
   }
 
@@ -233,87 +228,98 @@ public class Sketch extends PApplet {
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void drawQuestionPage() {
+
+    // load image
+    imgBackground = loadImage("Questions Page.jpg");
+
     // draw the background
     image(imgBackground, 0, 0);
 
-    if (blnPauseTimer == true) {
+    if (blnPauseTimer) {
       delay(1000);
-      previousTime = previousTime + 1000;
+      intPreviousTime += 1000;
       blnPauseTimer = false;
     }
 
-    int intCurrentTime = millis();
-    textSize(30);
     
-    String strTimer = nf(intCurrentTime - previousTime, 4);
-    System.out.println("++++"+strTimer);
-    textSize(50);
-    text(strTimer, 880, 60);
-    
-    if ((intCurrentTime - previousTime) > intTimer) {
-      textSize(20);
-      text("Time is up, move to next question", 450, 180);
-      blnPauseTimer = true;
-      intQuestionNumber++;
-      if (areQuestionsFinished() == true) {
-        delay(1000);
-        intPage = 2;
+
+
+    if (intTimer > 0) {
+      int intCurrentTime = millis();
+      int intElapsedSeconds = (int) ((intCurrentTime - intPreviousTime) / 1000); 
+      textSize(50);
+      text(intElapsedSeconds, 880, 60); 
+      if (intElapsedSeconds > intTimer / 1000) { 
+        textSize(30);
+        text("Time is up, move to next question!", 80, 160);
+        blnPauseTimer = true;
+        intQuestionNumber++;
+        if (areQuestionsFinished()) {
+          delay(1000);
+          intPage = 4;
+        }
+        intPreviousTime = millis();
+        return;
       }
-      previousTime = millis();
-      return;
     }
+
+  
 
     // print the questions
     fill(255);
     textSize(22);
     if (intHistoryOrGeography == 0) {
       text(strHistoryQuestions[intQuestionNumber], 80, 80);
-    } else {
+    } 
+    else if(intHistoryOrGeography == 1) {
       text(strGeographyQuestions[intQuestionNumber], 80, 80);
+    }
+    else if(intHistoryOrGeography == 2) {
+      text(strPopQuestions[intQuestionNumber], 80, 80);
+    }
+    else if(intHistoryOrGeography == 3) {
+      text(strMixedQuestions[intQuestionNumber], 80, 80);
     }
     
     // print options
     int intTextPosition = 140;
-    textSize(30);
+    textSize(20);
     for (int intColumn = 0; intColumn < strHistoryOptions[0].length; intColumn++) {
       if (intHistoryOrGeography == 0) {
         text(strHistoryOptions[intQuestionNumber][intColumn], 80, intTextPosition);
-      } else {
+      } 
+      else if (intHistoryOrGeography == 1) {
         text(strGeographyOptions[intQuestionNumber][intColumn], 80, intTextPosition);
+      }
+      else if(intHistoryOrGeography == 2) {
+        text(strPopOptions[intQuestionNumber][intColumn], 80, intTextPosition);
+      }
+      else if(intHistoryOrGeography == 3) {
+        text(strMixedOptions[intQuestionNumber][intColumn], 80, intTextPosition);
       }
       intTextPosition += 30;
     }
-    
 
-    // print red button
-    fill(255, 0, 0);
-    rect(0, 300, 500, 150, 30);
-    fill(255, 255, 255);
-    textSize(75);
-    text("A", 225, 400);
+  }
 
-    // print green button
-    fill(0, 255, 0);
-    rect(500, 300, 500, 150, 30);
-    fill(255, 255, 255);
-    textSize(75);
-    text("B", 725, 400);
+  public void drawFinalPage() {
 
-    // print blue button
-    fill(0, 0, 255);
-    rect(0, 450, 500, 150, 30);
-    fill(255, 255, 255);
-    textSize(75);
-    text("C", 225, 550);
+    imgEndingBackground = loadImage ("Ending Page.jpg");
+    image(imgEndingBackground, 0, 0);
 
-    // print yellow button
-    fill(240, 224, 58);
-    rect(500, 450, 500, 150, 30);
-    fill(255, 255, 255);
-    textSize(75);
-    text("D", 725, 550);
+    intHistoryOrGeography = 0; 
+    intTimer = 2000;
+    intQuestionNumber = 0;
 
-    // set timer
+    // print the questions
+    fill(255);
+    textSize(50);
+    if (intScore == 0) {
+      text(intScore + "%", 460, 210);
+    }
+    else {
+    text(intScore + "0%", 460, 210);
+    }
   }
 
   public void mousePressed() {
@@ -321,56 +327,85 @@ public class Sketch extends PApplet {
     if (intPage == 0) {
       mousePressedOpeningPage();
     }
-    //if (intPage == 1) {
-      
-    //}
-    if (intPage == 2) {
-      mousePressedStartingPage();
+    else if (intPage == 1) {
+      mousePressedTopicsPage();
     }
-    if (intPage == 3) {
+    else if (intPage == 2) {
+      mousePressedTimerPage();
+    }
+    else if (intPage == 3) {
       mousePressedQuestionPage();
     }
-    if (intPage == 4) {
+    else if (intPage == 4) {
       mousePressedFinalPage(); 
     }
       
   }
 
   public void mousePressedOpeningPage() {
+
     intPage = 1;
   }
 
   public void mousePressedTopicsPage(){
 
+    if ((mouseX >= 210 && mouseX <= 310) && (mouseY >= 232 && mouseY <= 332)) {
+
+      intHistoryOrGeography = 0;
+
+      intPage = 2;
+    }
+    if ((mouseX >= 351 && mouseX <= 451) && (mouseY >= 232 && mouseY <= 332)) {
+
+      intHistoryOrGeography = 1;
+
+      intPage = 2;
+    }
+    if ((mouseX >= 495 && mouseX <= 595) && (mouseY >= 232 && mouseY <= 332)) {
+
+      intHistoryOrGeography = 2;
+
+      intPage = 2;
+
+    }
+    if ((mouseX >= 640 && mouseX <= 740) && (mouseY >= 232 && mouseY <= 332)) {
+
+      intHistoryOrGeography = 3;
+
+      intPage = 2;
+
+    }
+
   }
 
-  public void mousePressedStartingPage() {
-    // Georagphy
-    if ((mouseX >= 150 && mouseX <= 450) && (mouseY >= 150 && mouseY <= 210)) {
-      intHistoryOrGeography = 1;
-    } 
-    // history
-    else if  ((mouseX >= 600 && mouseX <= 900) && (mouseY >= 150 && mouseY <= 210)) {
-      intHistoryOrGeography = 0;
-    }
-    // 2s 
-    else if ((mouseX >= 150 && mouseX <= 300) && (mouseY >= 300 && mouseY <= 360)) {
-      intTimer = 2*1000;
-    } 
-    // 5s 
-    else if ((mouseX >= 350 && mouseX <= 500) && (mouseY >= 300 && mouseY <= 360)) {
-      intTimer = 5*1000;
-    }
-    // 10s
-    else if ((mouseX >= 550 && mouseX <= 700) && (mouseY >= 300 && mouseY <= 360)) {
-      intTimer = 10*1000;
-    }
-    // next page
-    else if ((mouseX >= 400 && mouseX <= 700) && (mouseY >= 500 && mouseY <= 560)) {
+  public void mousePressedTimerPage() {
+
+    if ((mouseX >= 248 && mouseX <= 398) && (mouseY >= 265 && mouseY <= 345)){
+
       intPage = 3;
-      previousTime = millis();
+      intTimer = 5 * 1000;
+
     }
+
+    if((mouseX >= 424 && mouseX <= 574) && (mouseY >= 265 && mouseY <= 345)) {
+      
+      intPage = 3;
+      intTimer = 10 * 1000;
+
+    }
+
+    if((mouseX >= 600 && mouseX <= 750) && (mouseY >= 265 && mouseY <= 345)) {
+
+      intPage = 3;
+      intTimer = -1;
+
+    }
+    
+    intPreviousTime = millis();
+
   }
+
+
 
   /**
    * Check if buttons are pressed and indicates if the correct is chosen
@@ -420,17 +455,17 @@ public class Sketch extends PApplet {
         intPage = 4;
       }
       blnPauseTimer = true;
-      previousTime = millis();
+      intPreviousTime = millis();
     }
   }
 
   public void mousePressedFinalPage() {
     // Play again
-    if ((mouseX >= 200 && mouseX <= 400) && (mouseY >= 300 && mouseY <= 400)) {
+    if ((mouseX >= 385 && mouseX <= 615) && (mouseY >= 327 && mouseY <= 387)) {
       intPage = 1;
     } 
     // quit
-    else if ((mouseX >= 600 && mouseX <= 800) && (mouseY >= 300 && mouseY <= 400)) {
+    else if ((mouseX >= 385 && mouseX <= 615) && (mouseY >= 410 && mouseY <= 470)) {
       exit();
     } 
   }
@@ -448,4 +483,6 @@ public class Sketch extends PApplet {
     }
     return false;
   }
+
+
 }
